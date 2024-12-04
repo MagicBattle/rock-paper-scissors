c9bfd01ec03493c6b094ffc64e399bfd7c668637
Michael Yeung <michaelyeung145@gmail.com>
2024-12-04 11:51:44 -0800

    Added progress up to step 5 in the project: Added the program to be able to play one game of rock-paper-scissors

--- index.js
+++ index.js
@@ -22,12 +22,32 @@ function getComputerChoice(choice){
 function getHumanChoice(choice){
     let result;
     result = prompt(`Please enter a choice: rock, paper, or scissors`)
-
+    result = result.charAt(0).toUpperCase() + result.slice(1);
     return result;
+} 
+
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice == computerChoice) {
+        return console.log(`It's a tie!`);
+    } 
+    else if((humanChoice == `Rock` && computerChoice == `Scissors`) || (humanChoice == `Paper` && computerChoice == `Rock`) || (humanChoice == `Scissors` && computerChoice == `Paper`)){
+        humanScore++;
+        return console.log(`You win!`);
+    } 
+    else{
+        computerScore++
+        return console.log(`You lose!`);
+    }
 }
 
-let humanScore;
-let computeScore;
+let humanScore = 0;
+let computeScore = 0;
+const humanSelection = getHumanChoice(); 
+const computerSelection = getComputerChoice(3);
 
-console.log(getComputerChoice(3));
-console.log(getHumanChoice());
+
+
+playRound(humanSelection, computerSelection);
+
+// console.log(getComputerChoice(3));
+// console.log(getHumanChoice());
